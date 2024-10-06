e8988abba847cc73a124be69543364771695f783
Mrityunjayyshukla <shuklamrityunjay60@gmail.com>
2024-10-07 01:40:26 +0530

    added color palette

---
+++ lib/color_palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LightModeColorPalette{
+  Color backgroundColor = const Color(0xfffbfbfe);
+  Color panelColor = const Color(0xff2f27ce);
+  Color textColor = const Color(0xff040316);
+}
+
+class DarkModeColorPalette{
+  Color backgroundColor = const Color(0xff010104);
+  Color panelColor = const Color(0xff3a31d8);
+  Color textColor = const Color(0xffeae9fc);
+}
+
